f2665195210daca02f9fa70ff531f55e719bd9b1
Tenz <Martenz@users.noreply.github.com>
2022-02-17 16:26:03 +0100

    updated bash to run locally app on mac

--- localserver.sh
+++ localserver.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo $'Platypus App - Python3 Local http.server             Local app started               Browser should open automatically or visit localhost:9000 to start application              Close this window to stop application.'
+python3 -m http.server 9000 &
 open "http://localhost:9000"
-python3 -m http.server 9000
