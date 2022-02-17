d2e85ddfe5eada0387f54594bf1708895a06d5cf
Tenz <Martenz@users.noreply.github.com>
2022-02-17 14:43:21 +0100

    local app dev and importing all css and js

---
+++ localserver.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo $'Platypus App - Python3 Local http.server             Local app started               Browser should open automatically or visit localhost:9000 to start application              Close this window to stop application.'
+open "http://localhost:9000"
+python3 -m http.server 9000
